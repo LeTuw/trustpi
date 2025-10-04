39cc25914f8686ddd80c76e6c3f855c6ffd4e3e4
LeTuw <48324854+LeTuw@users.noreply.github.com>
2025-10-04 02:46:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,13 @@ app.config["SESSION_COOKIE_SECURE"] = True  # if HTTPS
 def make_session_non_permanent():
     session.permanent = False
 
+@app.before_request
+def log_request_info():
+    print("➡️ Incoming request path:", request.path)
+    print("➡️ Full URL:", request.url)
+    print("➡️ Method:", request.method)
+
+
 Session(app)
 CORS(app)
 
@@ -797,3 +804,4 @@ def health_check():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
